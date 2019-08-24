cb73686d3c40c6ba0b554531de3eb7bada00ee19
Raphael <raphaelkcc@gmail.com>
2019-08-24 11:47:52 -0300

    commit3

--- src/app/modal/modal.page.ts
+++ src/app/modal/modal.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal',
@@ -9,10 +9,11 @@ import { ModalController } from '@ionic/angular';
 
 export class ModalPage implements OnInit {
 
-  constructor(public modalController: ModalController) { }
+  constructor(public modalController: ModalController,public alertController: AlertController) { }
   peso:any;
   altura:any;
   imc:any;
+  status:any;
   ngOnInit() {
   }
   async closeModal(){
@@ -20,11 +21,42 @@ export class ModalPage implements OnInit {
     await this.modalController.dismiss(onClosedData);
 
   }
-enviadados(){
-  console.log(this.peso);
-  console.log(this.altura);
-this.imc=this.peso/(this.altura*this.altura);
-console.log(this.imc);
+  async enviadados(){
+    console.log(this.peso);
+    console.log(this.altura);
+    this.imc=this.peso/(this.altura*this.altura);
+    console.log(this.imc);
+      if(this.imc<20){
+        this.status="Abaixo do peso";
+      }
+      if(this.imc>=20 && this.imc<=24){
+        this.status="Peso normal";
+      }
+      if(this.imc>=24.1 && this.imc<=29){
+        this.status="Acima do peso";  
+      }
+      if(this.imc>=29.1 && this.imc<=34){
+        this.status="Obesidade";
+      }
+      if(this.imc>=34.1){
+        this.status="Obesidade Morbida";  
+      }
+
+ console.log(this.status);
+
+ const alert = await this.alertController.create({
+  header: 'Seu índice de massa corpórea é:',
+  subHeader: this.imc,
+  message: this.status,
+  buttons: [{
+    text:'OK',
+    handler:()=>{
+      this.closeModal();
+    }
+  }]
+});
+
+await alert.present();
 }
 
 }
